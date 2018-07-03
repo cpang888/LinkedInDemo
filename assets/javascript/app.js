bf9ecfd188c16b077f0b12bdd304dce7f6a8d94d
Cindy Pang <cipang@coca-cola.com>
2018-07-02 21:31:31 -0400

    test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,32 @@
 // Setup an event listener to make an API call once auth is complete
 function onLinkedInLoad() {
   console.log("onLinkedInLoad")
- IN.Event.on(IN, "auth", getProfileData);
+  IN.Event.on(IN, "auth", getProfileData);
+
+  $( "#logout" ).click(function() {
+    console.log("onclick");
+    logout();
+  });
+
+  function logout(){
+    console.log("LinkedIn logout");
+    IN.User.logout(onLogout);
+  }
+
+  function onLogout(){
+    // window.location = "/logout";
+    console.log('Logout successfully');
+  }
 }
 
 // Logout user
-function logout(){
-  IN.User.logout(onLogout);
-}
+// function logout(){
+//   IN.User.logout(onLogout);
+// }
 
-function onLogout(){
-  console.log('Logout successfully');
-}
+// function onLogout(){
+//   console.log('Logout successfully');
+// }
 
 // Use the API call wrapper to request the member's basic profile data
 function getProfileData() {
@@ -22,20 +37,6 @@ function getProfileData() {
   var userdata = data.values[0];
 
   console.log(userdata);
-//   $.ajax({
-//   //  url: "saveuser.php",
-//    url: "https://api.linkedin.com/v2/me",
-//    type: "post",
-//    data: {"social_type": "linkedin","fname": userdata.firstName,"lname": userdata.lastName,"email": userdata.emailAddress, "profile_image": userdata.pictureUrl },
-//    success: function(response){
-
-//     $('#tableUser').css('display','block');
-//     $('#fullname').text( userdata.firstName + " " + userdata.lastName);
-//     $('#email').text( userdata.emailAddress );
-//     $('#profile_photo').attr( 'src',userdata.pictureUrl );
-//     logout();
-//    }
-//  });
 
 }).error(function (data) {
   console.log(data);
