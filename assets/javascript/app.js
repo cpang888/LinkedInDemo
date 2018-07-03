3fce425b37c3099b633ea7cc936e100a6951641d
Cindy Pang <cipang@coca-cola.com>
2018-07-03 17:30:25 -0400

    test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,30 +6,35 @@
     function onLinkedInLoad() {
       console.log("onLinkedInLoad")
 
-      $( "#logout" ).click(function() {
-        console.log("onclick");
-        logout();
-      });
+      // $( "#logout" ).click(function() {
+      //   console.log("onclick");
+      //   logout();
+      // });
 
-      function logout(){
-        console.log("LinkedIn logout");
-        IN.User.logout(onLogout);
-      }
+      // function logout(){
+      //   console.log("LinkedIn logout");
+      //   IN.User.logout(onLogout);
+      // }
 
-      function onLogout(){
-        // window.location = "/logout";
-        console.log('Logout successfully');
-      }
+      // function onLogout(){
+      //   // window.location = "/logout";
+      //   console.log('Logout successfully');
+      // }
     }
 
     // Logout user
-    // function logout(){
-    //   IN.User.logout(onLogout);
-    // }
+    function logout(){
+      IN.User.logout(onLogout);
+    }
 
-    // function onLogout(){
-    //   console.log('Logout successfully');
-    // }
+    function onLogout(){
+      console.log('Logout successfully');
+    }
+
+    $( "#logout" ).click(function() {
+      console.log("onclick");
+      logout();
+    });
 
     // Use the API call wrapper to request the member's basic profile data
     function getProfileData() {
