bfd4d94e40727cb37b58e0c73f003f74d7fe4b19
Cindy Pang <cipang@coca-cola.com>
2018-07-04 08:43:11 -0400

    test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,80 +31,62 @@
 
         console.log(userdata);
 
-
-        // IN.API.PeopleSearch().fields("first-name", "last-name", "picture-url")
-        // .params({"keywords": "princess", "count":10, "sort":"distance"})
-        // .result(function (result) {
-          
-        //   // var userdata = data.values[0];
-
-        //   console.log(JSON.stringify(result));
-        // }).error(function (data) {
-        //   console.log(data);
-        // });
-
         console.log(userdata.positions[0]);
         // var govJobsUrl = "https://jobs.search.gov/jobs/search.json?query=%Technology%+jobs+in+Atlanta,Georgia";
 
-        var linkedInUrl = "https://api.linkedin.com/v2/recommendedJobs?q=byMember";
-        $.ajax({url: linkedInUrl,
-          type: 'GET',
-          contentType: "application/json",
-          // Access-Control-Allow-Origin: "GET",
-          // headers: header, /* pass your header object */
-          dataType: 'jsonp',
-          async: false,
-          // headers: {},
-          success: function(data) {
-            console.log("success");
-            console.log(data);
-          },
-          error: function(err) {
-            console.log('Error', err);
-          },
-        }).then(function(response) {
-          console.log(response);
-      });
-
-        // IN.API.Raw('/people/~:(recommendations-received)')
-        // IN.API.Raw('/people/~:(id,positions)?format=json')
-				// .method('GET')
-				// .result(function(result){
-        //   console.log(result);
-        // });
-         
-        var githubURL = "https://jobs.github.com/positions?description=technology&location=atlanta";
-        $.ajax({url: githubURL,
-          type: 'GET',
-          contentType: "application/json",
-          dataType: 'jsonp',
-          success: function(data) {
-            console.log("success");
-            console.log(data);
-          },
-          error: function(err) {
-            console.log('Error', err);
-          },
-        }).then(function(response) {
-          console.log(response);
-      });
-
-        var queryURL = "https://jobs.search.gov/jobs/search.json?query=%Technology%+jobs+in+Atlanta,Georgia";
-        // var queryURL = "https://api.linkedin.com/v2/recommendedJobs?q=byMember";
-
-        $.ajax({
-          url: queryURL,
-          method: "GET",
-          contentType: "application/json",
-          dataType: 'jsonp'
-        }).then(function(response) {
-            console.log(response);
-        });
 
       }).error(function (data) {
         console.log(data);
       });
     }
+
+    var linkedInUrl = "https://api.linkedin.com/v2/recommendedJobs?q=byMember";
+    $.ajax({url: linkedInUrl,
+      type: 'GET',
+      contentType: "application/json",
+      // Access-Control-Allow-Origin: "GET",
+      // headers: header, /* pass your header object */
+      dataType: 'jsonp',
+      async: false,
+      // headers: {},
+      success: function(data) {
+        console.log("success");
+        console.log(data);
+      },
+      error: function(err) {
+        console.log('Error', err);
+      }
+    }).then(function(response) {
+      console.log(response);
+    });
+
+    var githubURL = "https://jobs.github.com/positions?description=technology&location=atlanta";
+    $.ajax({url: githubURL,
+      type: 'GET',
+      contentType: "application/json",
+      dataType: 'jsonp',
+      success: function(data) {
+        console.log("success");
+        console.log(data);
+      },
+      error: function(err) {
+        console.log('Error', err);
+      }
+    }).then(function(response) {
+      console.log(response);
+    });
+
+    var queryURL = "https://jobs.search.gov/jobs/search.json?query=%Technology%+jobs+in+Atlanta,Georgia";
+    // var queryURL = "https://api.linkedin.com/v2/recommendedJobs?q=byMember";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      contentType: "application/json",
+      dataType: 'jsonp'
+    }).then(function(response) {
+        console.log(response);
+    });
   }
 
   $(document).ready(function() {
